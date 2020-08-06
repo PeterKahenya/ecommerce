70a2d1a86f90a42c6f614600c913aed741217afc
Njoki Peter Kahenya <33977993+PeterKahenya@users.noreply.github.com>
2020-08-06 11:24:29 +0300

    Added related fields to serializers

--- shop/serializers/products.py
+++ shop/serializers/products.py
@@ -1,35 +1,54 @@
 from rest_framework import serializers
+from .orders import OrderItemSerializer
 from ..models import Category,Supplier,Tag,Product,ProductDetail,DetailName,Review
 
 class ProductSerializer(serializers.ModelSerializer):
-    # product_details = serializers.HyperlinkedIdentityField(view_name="shop:product-details")
+    product_details = ProductDetailSerializer(many=True, read_only=True)
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['product_details','reviews','order_items','id','serial_no','name','category','image','description','price','supplier','tags','created_at','updated_at']
         
 class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id','name','products']
 
 class SupplierSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Supplier
-        fields = '__all__'
+        fields = ['id','products','title','image','created_at','updated_at']
+        
 
 class TagSerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = ['id','products','title','created_at','updated_at']
+        
 
 class DetailNameSerializer(serializers.ModelSerializer):
+    details = ProductDetailSerializer(many=True, read_only=True)
+    
     class Meta:
         model = DetailName
-        fields = '__all__'
+        fields = ['id','name','details','created_at','updated_at']
         
-        
-class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+class ProductDetailSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = ProductDetail
+        fields = '__all__'       
+        
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
         fields = '__all__'
         
