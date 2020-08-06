4e1944ea9c59b0917fcef91ce21aab78c085093c
Njoki Peter Kahenya <33977993+PeterKahenya@users.noreply.github.com>
2020-08-06 13:13:21 +0300

    Created orders.py

---
+++ shop/viewsets/orders.py
@@ -0,0 +1,51 @@
+from shop.models import Order,OrderItem
+from shop.serializers.products import ProductSerializer, CategorySerializer
+
+
+class AllOrdersListView(APIView):
+    """
+    List all products, or create a new product.
+    """
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+      
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  class MyOrderView(APIView):
+    """
+    List all products, or create a new product.
+    """
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+      
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+  class ModifyCart(APIView):
+    """
+    Adding an item to cart or removing or changing the quantity
+    """
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+      
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
